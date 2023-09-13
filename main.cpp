ada63db1feb1ec2ef11acbd755808b9b142cfd11
davinchavarria <davin.chavarria@udea.edu.co>
2023-09-13 16:40:11 -0500

    Se agrega el problema_14

--- main.cpp
+++ main.cpp
@@ -175,6 +175,7 @@ Nota: un ejemplo de cuadrado mágico es el siguiente:
 |8|1|6|
  -----
 */
+/*
 bool valDupli(int arr[], int n) {// comprobar valotres repetidos
     bool iguales = true;
     for(int i = 0; i < n-1; i++) {
@@ -259,4 +260,34 @@ int main() {
         cout<<"No es Cuadrado Magic1o";
     }
 
+}
+*/
+/*
+Problema 14. Elabore un programa que llene una matriz 5x5 con los números del 1 al 25 y la imprima, luego
+imprima la matriz rotada 90, 180 y 270 grados.
+*/
+void rotar(int matriz[5][5],int matrizrotar[5][5]){// funcion para rotar matriz 90 grados
+    for(int i = 4,io=0,val=1;i>=0; i--,io++){
+        for(int j= 0,jo=0;j<5; j++,jo++){
+            matrizrotar[j][i]=matriz[io][jo];// escribir desde la ultima columna hasta la 1
+        }
+    }for(int i = 0;i<5; i++){cout<<endl;// se imprime la matriz rotada
+        for(int j= 0;j<5; j++){
+            cout<<matrizrotar[i][j]<<" ";
+        }
+    }cout<<endl;
+}
+int main(){
+ int matriz[5][5];int matriz9[5][5];
+ int matriz18[5][5];int matriz27[5][5];
+ for(int i = 0,val=1;i<5; i++){// creamos matriz origina con valores de 1-25 en orden
+  for(int j= 0;j<5; j++){
+   matriz[i][j]=val;val++;
+  }
+ }for(int i = 0;i<5; i++){cout<<endl;// se imprime matriz origin
+        for(int j= 0;j<5; j++){
+            cout<<matriz[i][j]<<" ";
+        }
+    }cout<<endl;
+ rotar(matriz,matriz9); rotar(matriz9,matriz18); rotar(matriz18,matriz27);
 }
